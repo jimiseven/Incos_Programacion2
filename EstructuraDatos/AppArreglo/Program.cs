d38ed4cc64f787ec204bd658a2ce3327a333d1c5
LaptopTarksLocal <tano777864@gmail.com>
2023-09-05 19:12:22 -0400

    arreglos basicos

---
+++ EstructuraDatos/AppArreglo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppArreglo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //llamaremos a este objeto
+            //tipo de arreglo con los numero de datos insertados
+            //las propiedades aparecen con llavecitas
+            int[] arregloTelefonos = {7654765,45674567,4567456,45674567, 23423352};
+
+            for (int i = 0; i < arregloTelefonos.Length; i++)
+            {
+                Console.WriteLine("Elemento en el índice " + i + ": " + arregloTelefonos[i]);
+            }
+
+        }
+    }
+}
